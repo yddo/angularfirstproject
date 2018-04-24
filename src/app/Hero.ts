aca4d628674f3998ba401b70a9ceae3f37916fec
yddo <loker.yddo@gmail.com>
2018-04-24 12:46:21 +0300

    added hero compnent

---
+++ src/app/Hero.ts
@@ -0,0 +1,4 @@
+export class Hero {
+    id: 1;
+    name: 'windstrom';
+  }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../Hero';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
+
+
 export class HeroesComponent implements OnInit {
 
   constructor() { }
